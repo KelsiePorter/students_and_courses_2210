98e8f989389497206df4017a032aa39d4d21ed22
Kelsie Porter <krporter22@gmail.com>
2022-10-27 10:24:03 -0600

    Create #students_scrore_below_80

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -2,17 +2,31 @@ require 'pry'
 
 class Gradebook 
   attr_reader :instructor,
-              :courses,
-              :all_students
+              :courses
+              # :all_students
 
   def initialize(instructor)
     @instructor = instructor
     @courses = []
-    @all_students = []
+    # @all_students = []
   end
 
   def add_course(course)
     @courses << course
   end  
+
+  def all_students 
+    students = @courses.map do |course|
+      course.students
+    end
+    students.flatten!
+  end
+
+  def students_score_below_80
+    students_below_80 = all_students.select do |student|
+      student.grade < 80.0
+    end
+    students_below_80
+  end
 
 end
