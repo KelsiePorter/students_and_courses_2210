c9a018aad069a97f7bc9a3953dfa010b0ab9cdda
Kelsie Porter <krporter22@gmail.com>
2022-10-27 09:15:42 -0600

    Create Student class and associated methods

---
+++ lib/student.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Student 
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(student_hash)
+    @name = student_hash[:name]
+    @age = student_hash[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score.to_f
+  end
+
+  def grade 
+    @scores.sum / @scores.size
+  end
+
+end
